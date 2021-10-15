aa2ffee15b1e07cd3f73b0f4fcfa7715953131a0
Caprena Richie <robcaprena@gmail.com>
2021-10-15 15:22:53 -0400

    first commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var videoGameNames = new List<string>()
+            {
+                "NBA2K21",
+                "Madden 22",
+                "MLBTheShow 22",
+                "Forza Horizon 3",
+            };
+            var stringList = videoGameNames.OrderBy(vidgam => vidgam.Length).ToList();
+            Console.WriteLine("List of Video Games");
+            Console.WriteLine("======================");
+            videoGameNames.ForEach(vidgam => Console.WriteLine(vidgam));
+            
+        }
+    }
+}
